c0a38aa7f4c0bcc7c3f931f71148799d9ac30748
Zoombar <69800759+Zoombar@users.noreply.github.com>
2022-05-18 19:21:57 +0300

    1.0.2
    There are two players. Control of the first: "W" up "S" down .
    Control of the second: "upper arrow" - up "bottom arrow" - down

--- ping_pong.py
+++ ping_pong.py
@@ -2,7 +2,7 @@ from pygame import *
 window = display.set_mode((700, 500))
 display.set_caption('Ping Pong')
 background = transform.scale(image.load('background.jpg'), (700, 500))
-
+win_height = 500
 clock = time.Clock()
 FPS = 60
 
@@ -19,22 +19,22 @@ class GameSprite(sprite.Sprite):
        window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def updatel(self):
-        keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-    def updater(self):
-        keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-                
-    def fire(self):
-        bullet = Bullet('bullet.png',self.rect.centerx, self.rect.top, 15, 20, -15)
-        bullets.add(bullet)
+   def update_r(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 150:
+           self.rect.y += self.speed
+   def update_l(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 150:
+           self.rect.y += self.speed
+
+
+player1 = Player('wall.png', 30, 200,50,150, 4)
+player2 = Player('wall.png', 630, 200,50,150, 4)
 
 game = True
 
@@ -44,6 +44,9 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-
+    player1.update_l()
+    player1.reset()
+    player2.update_r()
+    player2.reset()
     display.update()
     clock.tick(FPS)
