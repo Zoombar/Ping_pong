d80c2d20729a93ec73c1564d79af245366085cdd
Zoombar <69800759+Zoombar@users.noreply.github.com>
2022-05-18 18:58:05 +0300

    version 1.01
    new class(not playable)

--- ping_pong.py
+++ ping_pong.py
@@ -6,6 +6,36 @@ background = transform.scale(image.load('background.jpg'), (700, 500))
 clock = time.Clock()
 FPS = 60
 
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       sprite.Sprite.__init__(self)
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ 
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def updatel(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+    def updater(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+                
+    def fire(self):
+        bullet = Bullet('bullet.png',self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+
 game = True
 
 while game:
